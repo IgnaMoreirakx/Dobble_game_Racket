40813f6f640f91c59ebe55458a020c4f38da1d01
ignacio Moreira <ignacio.moreira@usach.cl>
2022-04-13 22:05:13 -0400

    se crean funciones nuevas y separador de archivo
    se crean las funciones dobble?, numcard y se crea el archivo funciones auxiliares

--- TDA_CardsSet_21318366_MoreiraCarvacho.rkt
+++ TDA_CardsSet_21318366_MoreiraCarvacho.rkt
@@ -1,6 +1,7 @@
 #lang racket
 (define Simbolos (list 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24))
 (define letras (list "a" "b" "c" "d" "e" "f" "g" "h"))
+(require "funciones_auxiliares.rkt")
 ;Este tda corresponde a al representancion del mazo de cartas de dobble
 ;se busca abarcar el crear un mazo valido de dobble con "cartas"
 ;conociendo las restricciones matematicas de dobble
@@ -15,37 +16,26 @@
 ;Dominio: Lista de elementos o simbolos y un entero (e) que significa la cantidad de simbolos por carta y un entero c que significa la cantidad de cartas a entregar
 ;recorrido: Cardsset
 ;recursividad: Natural
-(define CardsSet
-  (lambda (e c elementos fn)
+(define Finallyset
+  (lambda (e c elementos)
     (define calcular
       (lambda (e c lista)
-        (if (primo? (- e 1))
+        (if (or (primo? (- e 1)) (= (- e 1) 1))
             (if (<= c -1)
                 lista
-                (if (= c 0)
+                (if (or (= c 0) (> c (+ (* (- e 1)(- e 1)) (- e 1) 1)))
                     null
                     (cons (car lista) (calcular e (- c 1) (cdr lista)))))
             null)))
      (calcular e c (mazo e elementos))))
 
-
-;Fuunción: verifica si un numero es primo o no
-;Dominio: entero
-;recorrido: boolean
-;recursividad: de cola
-(define primo?
-  (lambda (x)
-    (define calcular
-      (lambda (x cont acum)
-        (if (and (>= x cont) (= (remainder x cont) 0))
-            (calcular x (+ cont 1) (+ acum 1))
-            (if (>= x cont)
-                (calcular x (+ cont 1) acum)
-                (if (= acum 2)
-                    #t
-                    #f)))))
-    (calcular x 1 0)))
-
+;esta función irá en el main
+(define CardsSet (lambda ( e c elementos fn)
+                   (if (not(comparar elementos))
+                       null
+                       (if (odd? fn)
+                           (shuffle (Finallyset e c elementos))
+                           (shuffle (shuffle (Finallyset e c elementos)))))))
 
 ;Fuunción: es la encargada de crear el mazo en su totalidad
 ;Dominio: Lista de elementos o simbolos y un entero (e) que significa la cantidad de simbolos por carta y un entero c que significa la cantidad de cartas a entregar
@@ -135,51 +125,26 @@
             (calcular (append lista (Finally_cards e (+ i 1) j elementos)) (+ i 1) j))))
     (calcular (Finally_cards e i j elementos) i j)))
 
-;las siguientes funciones corresponden a la transformación de los numeros de las cartas a elementos de una lista,
-;la cual es parametro de la función principal de este construcctor
-;Funciones para transformar numeros en elementos de una lista
 
-;Funcion: funcion para comparar los elementos de la lista con los numeros
-;Dominio: 2 enteros, uno corresponde a los elementos de la lista y el otro un numero entero
-;Recorrido:  boolean, t si son iguales, f si son distintos
-;recursion; no aplica
-(define i?
-  (lambda (e i)
-    (define calcular
-      (lambda ( e i)
-        (if (equal? i e)
-            #t
-            #f)))
-    (calcular e i)))
+;Funcion: Verifica que un set sea válido
+;Dominio: CardsSet
+;Recorrido:  boolean
+;recursion; No aplica
+(define dobble? (lambda (lista)
+                  (if (null? lista)
+                      #f
+                      #t)))
 
-
-;Funcion: Hace un cambio de los elementos si estos son iguales
-;Dominio: 2 listas y una función de comparación
-;Recorrido:  lista con los elementos cambiados
-;recursion; Natural
-
-
-(define myfilter2
-  (lambda ( lista funct lista2)
-    (define calcular
-      (lambda (lista funct i lista2)
-                     (if (or (empty? lista)(empty? lista2))
-                         null
-                         (if (funct (car lista) i)
-                             (cons (car lista2) (calcular (cdr lista) funct i lista2))
-                             (calcular lista funct (+ i 1) (cdr lista2))))))
-    (calcular lista funct 1 lista2)))
-
-;Funcion: Agrupa las cartas ya con los simbolos cambiados
-;Dominio: 2 listas, una seran las cartas y el otro los elemetos que se quieran que esten en las cartas
-;Recorrido:  genera las cartas con los simbolos que se requieren
+;Funcion: Contar la cantidad de cartas en el set
+;Dominio: CardsSet
+;Recorrido:  número de cartas (int)
 ;recursion; De cola
-(define transformar
-  (lambda (cartas elementos)
-    (define aplicar
-      (lambda (cartas funct elementos)
-        (myfilter2 cartas funct elementos)))
-    (aplicar cartas i? elementos)))
+(define numCards (lambda (lista)
+                   (define aux (lambda (lista cont)
+                                 (if (null? lista)
+                                     cont
+                                     (aux (cdr lista) (+ 1 cont)))))
+                   (aux lista 0)))
 
 
 ;Funcion: Encargadad de entregar la N-esíma carta
@@ -215,3 +180,11 @@
     (if (primo? (- (length lista) 1))
         (+ (* (- (length lista) 1) (- (length lista) 1)) (- (length lista) 1) 1)
         null)))
+
+
+
+
+
+
+
+
---
+++ funciones_auxiliares.rkt
@@ -0,0 +1,84 @@
+#lang racket
+(provide (all-defined-out))
+
+
+
+;Funcion: funcion para comparar los elementos de la lista con los numeros
+;Dominio: 2 enteros, uno corresponde a los elementos de la lista y el otro un numero entero
+;Recorrido:  boolean, t si son iguales, f si son distintos
+;recursion; no aplica
+(define i?
+  (lambda (e i)
+    (define calcular
+      (lambda ( e i)
+        (if (equal? i e)
+            #t
+            #f)))
+    (calcular e i)))
+
+;Funcion: Hace un cambio de los elementos si estos son iguales
+;Dominio: 2 listas y una función de comparación
+;Recorrido:  lista con los elementos cambiados
+;recursion; Natural
+
+
+(define myfilter2
+  (lambda ( lista funct lista2)
+    (define calcular
+      (lambda (lista funct i lista2)
+                     (if (or (empty? lista)(empty? lista2))
+                         null
+                         (if (funct (car lista) i)
+                             (cons (car lista2) (calcular (cdr lista) funct i lista2))
+                             (calcular lista funct (+ i 1) (cdr lista2))))))
+    (calcular lista funct 1 lista2)))
+
+
+;Funcion: Agrupa las cartas ya con los simbolos cambiados
+;Dominio: 2 listas, una seran las cartas y el otro los elemetos que se quieran que esten en las cartas
+;Recorrido:  genera las cartas con los simbolos que se requieren
+;recursion; De cola
+(define transformar
+  (lambda (cartas elementos)
+    (define aplicar
+      (lambda (cartas funct elementos)
+        (myfilter2 cartas funct elementos)))
+    (aplicar cartas i? elementos)))
+
+
+;Fuunción: verifica si un numero es primo o no
+;Dominio: entero
+;recorrido: boolean
+;recursividad: de cola
+(define primo?
+  (lambda (x)
+    (define calcular
+      (lambda (x cont acum)
+        (if (and (>= x cont) (= (remainder x cont) 0))
+            (calcular x (+ cont 1) (+ acum 1))
+            (if (>= x cont)
+                (calcular x (+ cont 1) acum)
+                (if (= acum 2)
+                    #t
+                    #f)))))
+    (calcular x 1 0)))
+;-------------------------------------------------------------
+
+(define comparar (lambda (lista)
+                   (define aux (lambda (lista lista2)
+                                 (if (or (= (length lista) 1) (null? lista))
+                                     #t
+                                     (if (null? lista2)
+                                         (aux (cdr lista) (cdr (cdr lista)))
+                                     (if (= (car lista) (car lista2))
+                                         #f
+                                         (aux lista (cdr lista2)))))))
+                   (aux lista (cdr lista))))
+;---------------------------------------------------------
+(define m 2147483647)
+(define a 1103515245)
+(define c 12345)
+(define randomFn (lambda (xn)
+                   (modulo (+ (* a xn) c) m)
+                 )
+)
